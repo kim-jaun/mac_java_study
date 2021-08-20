7a180d44870177e787a3330832a895ec75a78948
kim-jaun <67224239+kim-jaun@users.noreply.github.com>
2021-08-20 16:57:06 +0900

    study_commit

---
+++ javaSrc/ch03/src/ch03/For2.java
@@ -0,0 +1,12 @@
+package ch03;
+
+public class For2 {
+	public static void main(String[] args) {
+		// 1 ~ 10까지 합계를 구하라.
+		int num = 0;
+		for(int i = 1; i <= 10; i++) {
+			num += i;
+		}
+		System.out.println(num);
+	}
+}
---
+++ javaSrc/ch03/src/ch03/For3.java
@@ -0,0 +1,20 @@
+package ch03;
+
+public class For3 {
+	public static void main(String[] args) {
+		// evenSum, oddSum, 총합계 totSum 1~100까지 합계를 구하라.
+		int evensum = 0;
+		int oddsum = 0;
+		for (int i = 1; i <= 100; i++) {
+			if (i % 2 == 1) {
+				oddsum += i;
+			}
+			else {
+				evensum += i;
+			}
+		}
+		System.out.println("짝수 합계 : " + evensum);
+		System.out.println("홀수 합계 : " + oddsum);
+		System.out.println("총 합계 : " + (evensum + oddsum));
+	}
+}
