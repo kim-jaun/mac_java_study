97079fdf38cf4d8ac48f2bb846b90a7cf6ec850e
kim-jaun <67224239+kim-jaun@users.noreply.github.com>
2021-08-23 09:01:40 +0900

    Update For4.java

--- javaSrc/ch03/src/ch03/For4.java
+++ javaSrc/ch03/src/ch03/For4.java
@@ -1,20 +1,20 @@
 package ch03;
-
+import java.util.Scanner;
 public class For4 {
 	public static void main(String[] args) {
-		// evenSum, oddSum, 총합계 totSum 1~100까지 합계를 구하라.
-		int evensum = 0;
-		int oddsum = 0;
-		for (int i = 1; i <= 100; i++) {
-			if (i % 2 == 1) {
-				oddsum += i;
-			}
-			else {
-				evensum += i;
-			}
-		}
-		System.out.println("짝수 합계 : " + evensum);
-		System.out.println("홀수 합계 : " + oddsum);
-		System.out.println("총 합계 : " + (evensum + oddsum));
+		Scanner sc = new Scanner(System.in);
+		do {
+			System.out.println("숫자를 입력하세요");
+			int num = Integer.parseInt(sc.nextLine());
+			if (num == 0) break;
+			else if (num >=2 && num <= 9) {
+				System.out.println("구구단 " + num + "단");
+				for (int i = 1; i < 10; i++) {
+					System.out.printf("%d * %d = %d\n",num,i, num*i);
+				}
+			} else System.out.println("구구단 숫자가 아닙니다.");
+		}while(true);
+		System.out.println("프로그램 종료.");
+		sc.close();
 	}
 }
