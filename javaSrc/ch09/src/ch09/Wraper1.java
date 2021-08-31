04390fa6b4803e52f24ae7fcaaec3eef1f20469b
kim-jaun <67224239+kim-jaun@users.noreply.github.com>
2021-08-31 16:18:04 +0900

    Update Wraper1.java

--- javaSrc/ch09/src/ch09/Wraper1.java
+++ javaSrc/ch09/src/ch09/Wraper1.java
@@ -1,5 +1,17 @@
 package ch09;
 
 public class Wraper1 {
-
+	public static void main(String[] args) {
+		char[] obj = {'1','a','#','B'};
+		for(int i = 0; i < obj.length; i++) {
+			System.out.println("문자 [" + obj[i] + "]:");
+			if (Character.isDefined(obj[i])) System.out.println("유니코드");
+			if (Character.isDigit(obj[i])) System.out.println("숫자");
+			if (Character.isLowerCase(obj[i])) System.out.println("소문자");
+			if (Character.isUpperCase(obj[i])) System.out.println("대문자");
+			if (Character.isWhitespace(obj[i])) System.out.println("공백");
+		}
+		Integer i1 = new Integer(1);	// 기존 작성 방법.
+		Integer i2 = 2;					// 오토언박싱 된 새롭게 작성하는 방법.
+	}
 }
