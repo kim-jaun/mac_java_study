8a6ccb77285617e23711df3785159dc4f7b26f66
kim-jaun <67224239+kim-jaun@users.noreply.github.com>
2021-09-12 17:58:44 +0900

    commit

--- javaSrc/ch17/src/ch17/TimeServer1.java
+++ javaSrc/ch17/src/ch17/TimeServer1.java
@@ -10,6 +10,8 @@ import java.util.GregorianCalendar;
 public class TimeServer1 {
 	public static void main(String[] args) throws IOException {
 		ServerSocket ss = new ServerSocket(7000);
+//		7000 포트번호
+		System.out.println("서버 가동 중");
 		try {
 			while(true) {
 				Socket client = ss.accept(); // 클라이언트가 접속하기를 기다리는 중
