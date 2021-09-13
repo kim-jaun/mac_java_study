07d7a1b9047c1ab6823454d0ad7e968b7ff8414f
kim-jaun <67224239+kim-jaun@users.noreply.github.com>
2021-09-13 16:49:24 +0900

    commit

--- javaSrc/ch18/src/ch18/MyFuniter2Ex.java
+++ javaSrc/ch18/src/ch18/MyFuniter2Ex.java
@@ -1,5 +1,20 @@
 package ch18;
 
-public class MyFuniter2Ex {
+interface MyInter2 {
+	public int method(int x, int y);
+}
 
+public class MyFuniter2Ex {
+	public static void main(String[] args) {
+		MyInter2 mi = new MyInter2() {
+			public int method(int x, int y) { return x+y; }
+		};
+		System.out.println(mi.method(12, 23));
+//		매개변수가 있으면 매개변수명 사용
+		mi = (x,y) -> {	return x+y;	};
+		System.out.println(mi.method(11, 8));
+//		문장이 하나일경우에는 중괄호({})생략, return문만 있으면 return 생략
+		mi = (x,y) -> x+y;
+		System.out.println(mi.method(55, 23));
+	}
 }
