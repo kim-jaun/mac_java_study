04be356914aa3a9cac02f9623ef210bc6f0c8f33
kim-jaun <67224239+kim-jaun@users.noreply.github.com>
2021-09-29 10:48:10 +0900

    commit

--- javaSrc/helloWorld/src/helloWorld/test.java
+++ javaSrc/helloWorld/src/helloWorld/test.java
@@ -2,10 +2,11 @@ package helloWorld;
 
 public class test {
 	public static void main(String[] args){
-        System.out.println("\\    /\\");
-        System.out.println(" )  ( ')");
-        System.out.println("(  /  )");
-        System.out.println(" \\(__)|");
+        System.out.println("|\\_/|");
+        System.out.println("|q p|   /}");
+        System.out.println("( 0 )\"\"\"\\");
+        System.out.println("|\"^\"`    |");
+        System.out.println("||_/=\\\\__|");
     }
 }
 
