aacf472e5e1b20d68c1b2c56631488432eae6ad5
kim-jaun <67224239+kim-jaun@users.noreply.github.com>
2021-09-29 10:17:58 +0900

    commit

---
+++ javaSrc/helloWorld/src/helloWorld/test.java
@@ -0,0 +1,12 @@
+package helloWorld;
+
+public class test {
+	public static void main(String[] args){
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
+
+
