ff5caab7a7bdaf8d98278030c94784172b726411
kim-jaun <67224239+kim-jaun@users.noreply.github.com>
2021-10-02 22:23:38 +0900

    Update test.java

--- javaSrc/helloWorld/src/helloWorld/test.java
+++ javaSrc/helloWorld/src/helloWorld/test.java
@@ -8,11 +8,12 @@ public class test {
 		
 		int num1 = sc.nextInt();
 		
-		if (num1 >= 90 && num1 <= 100) System.out.println("A");
-		else if (num1 >= 80 && num1 <= 89) System.out.println("B");
-		else if (num1 >= 70 && num1 <= 79) System.out.println("C");
-		else if (num1 >= 60 && num1 <= 69) System.out.println("D");
-		else System.out.println("F");
+		if (num1%4 == 0) {
+			if(num1%400 == 0) System.out.println(1);
+			else if (num1%100 == 0) System.out.println(0);
+			else System.out.println(1);
+		}
+		else System.out.println(0);
 	}
 }
 
