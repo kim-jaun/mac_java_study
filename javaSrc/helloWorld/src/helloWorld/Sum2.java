0bbdcc28af8d50245cee6bed4045c4d8a5f51baf
kim-jaun <67224239+kim-jaun@users.noreply.github.com>
2021-10-04 23:50:09 +0900

    Update Sum2.java

--- javaSrc/helloWorld/src/helloWorld/Sum2.java
+++ javaSrc/helloWorld/src/helloWorld/Sum2.java
@@ -1,5 +1,25 @@
 package helloWorld;
 
-public class Sum2 {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
+public class Sum2 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int num = Integer.parseInt(br.readLine());
+		
+		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
+		
+		for (int i = 0; i < num; i++) {
+			st = new StringTokenizer(br.readLine());
+			sb.append(Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken()));
+			sb.append('\n');
+		}
+		
+		System.out.println(sb);
+	}
 }
