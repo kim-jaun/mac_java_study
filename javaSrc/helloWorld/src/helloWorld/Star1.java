9715918b28d276bedff121313cf5143ed4675a36
kim-jaun <67224239+kim-jaun@users.noreply.github.com>
2021-10-06 11:35:35 +0900

    commit

--- javaSrc/helloWorld/src/helloWorld/Star1.java
+++ javaSrc/helloWorld/src/helloWorld/Star1.java
@@ -2,19 +2,17 @@ package helloWorld;
 
 import java.util.Scanner;
 
-public class ABplus {
+public class Star1 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
 		int num = sc.nextInt();
 		
 		for (int i = 1; i <= num; i++) {
-			int A = sc.nextInt();
-			int B = sc.nextInt();
-			
-			System.out.println("Case #" + i + ": " + A + " + " + B + " = " + (A + B));
+			for (int k=1; k <= i; k++) {
+				System.out.print("*");
+			}
+			System.out.println();
 		}
-		
-		sc.close();
 	}
 }
