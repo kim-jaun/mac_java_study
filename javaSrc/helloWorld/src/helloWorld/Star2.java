a1bc1f5cc5a2f08e88fc49bf60ec6654acf45827
kim-jaun <67224239+kim-jaun@users.noreply.github.com>
2021-10-08 17:36:28 +0900

    Update Star2.java

--- javaSrc/helloWorld/src/helloWorld/Star2.java
+++ javaSrc/helloWorld/src/helloWorld/Star2.java
@@ -1,18 +1,29 @@
 package helloWorld;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Star2 {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		int num = sc.nextInt();
+		int num = Integer.parseInt(br.readLine());
+		
+		br.close();
+		
+		StringBuilder sb = new StringBuilder();
+		
 		
 		for (int i = 1; i <= num; i++) {
-			for (int k = 1; k <= i; k++) {
-				System.out.print("*");
+			for (int k = 1; k <= num - i; k++) {
+				sb.append(" ");
 			}
-			System.out.println();
+			for (int j = 1; j <= i; j++) {
+				sb.append("*");
+			}
+			sb.append('\n');
 		}
+		System.out.println(sb);
 	}
 }
